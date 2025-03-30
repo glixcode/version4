072df618da825724c17a83bf94d178a673f9e4b3
gil gucela <gcodeencrypt@gmail.com>
2025-03-30 14:40:38 +0800

    commit from other account

--- backend/server.js
+++ backend/server.js
@@ -8,12 +8,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT,() => {
-    console.log("hello world");
+    console.log(" Server Running");
 })
 
 app.get('/',(request, response)=> {
-
     response.status(200).json({success: true})
-
 })
+
 
